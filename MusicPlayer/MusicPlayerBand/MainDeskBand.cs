f31b0e6ab321b0d57b6201534c1c8082af41e0c2
Randy <Randy@c2a5c9a7-c13e-dc4e-b3ec-969ff3f223ac>
2011-07-22 01:50:45 +0000

    First commit
    git-svn-id: svn://localhost@3 c2a5c9a7-c13e-dc4e-b3ec-969ff3f223ac

---
+++ MusicPlayer/MusicPlayerBand/MainDeskBand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using BandObjectLib;
+using System.Runtime.InteropServices;
+
+namespace MusicPlayerBand
+{
+    [Guid("AE07101B-46D4-4a98-AF68-0333EA26E113")]//or generate my own guid using guidgen.exe...
+    [BandObject("Music Player Bar", BandObjectStyle.TaskbarToolBar | BandObjectStyle.ExplorerToolbar | BandObjectStyle.Horizontal, HelpText = "Controls to music.")]
+    public partial class MainDeskBand : BandObject
+    {
+        //fields
+        private System.Windows.Forms.Button playButton;
+
+        //constructor
+        public MainDeskBand()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("ZZZZZ Hello, World!");
+        }
+    }
+}
