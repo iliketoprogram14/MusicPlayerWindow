a17bbbe3bd4819f94b200fbbd0c9ff5e623a0025
Randy <Randy@c2a5c9a7-c13e-dc4e-b3ec-969ff3f223ac>
2011-07-28 03:40:40 +0000

    layout for the shuffler is set...
    git-svn-id: svn://localhost@29 c2a5c9a7-c13e-dc4e-b3ec-969ff3f223ac

--- MusicPlayerWindow/trunk/MusicPlayerWindow/MainWindow.Designer.cs
+++ MusicPlayerWindow/trunk/MusicPlayerWindow/MainWindow.Designer.cs
@@ -34,14 +34,19 @@
             this.nextButton = new System.Windows.Forms.Button();
             this.volumeBar = new System.Windows.Forms.TrackBar();
             this.playlistBox = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.songLabel = new System.Windows.Forms.Label();
+            this.artistAlbumLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // playButton
             // 
-            this.playButton.Location = new System.Drawing.Point(65, 27);
+            this.playButton.Location = new System.Drawing.Point(58, 12);
+            this.playButton.MinimumSize = new System.Drawing.Size(60, 60);
             this.playButton.Name = "playButton";
-            this.playButton.Size = new System.Drawing.Size(53, 23);
+            this.playButton.Size = new System.Drawing.Size(60, 60);
             this.playButton.TabIndex = 1;
             this.playButton.Text = "Play";
             this.playButton.UseVisualStyleBackColor = true;
@@ -50,9 +55,10 @@
             // stopButton
             // 
             this.stopButton.Enabled = false;
-            this.stopButton.Location = new System.Drawing.Point(124, 27);
+            this.stopButton.Location = new System.Drawing.Point(124, 12);
+            this.stopButton.MinimumSize = new System.Drawing.Size(60, 60);
             this.stopButton.Name = "stopButton";
-            this.stopButton.Size = new System.Drawing.Size(39, 23);
+            this.stopButton.Size = new System.Drawing.Size(60, 60);
             this.stopButton.TabIndex = 2;
             this.stopButton.Text = "Stop";
             this.stopButton.UseVisualStyleBackColor = true;
@@ -61,9 +67,11 @@
             // prevButton
             // 
             this.prevButton.Enabled = false;
-            this.prevButton.Location = new System.Drawing.Point(13, 27);
+            this.prevButton.Location = new System.Drawing.Point(13, 22);
+            this.prevButton.MaximumSize = new System.Drawing.Size(40, 40);
+            this.prevButton.MinimumSize = new System.Drawing.Size(40, 40);
             this.prevButton.Name = "prevButton";
-            this.prevButton.Size = new System.Drawing.Size(46, 23);
+            this.prevButton.Size = new System.Drawing.Size(40, 40);
             this.prevButton.TabIndex = 3;
             this.prevButton.Text = "Prev";
             this.prevButton.UseVisualStyleBackColor = true;
@@ -72,9 +80,9 @@
             // nextButton
             // 
             this.nextButton.Enabled = false;
-            this.nextButton.Location = new System.Drawing.Point(169, 27);
+            this.nextButton.Location = new System.Drawing.Point(190, 22);
             this.nextButton.Name = "nextButton";
-            this.nextButton.Size = new System.Drawing.Size(45, 23);
+            this.nextButton.Size = new System.Drawing.Size(40, 40);
             this.nextButton.TabIndex = 4;
             this.nextButton.Text = "Next";
             this.nextButton.UseVisualStyleBackColor = true;
@@ -82,31 +90,67 @@
             // 
             // volumeBar
             // 
-            this.volumeBar.Location = new System.Drawing.Point(12, 56);
+            this.volumeBar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.volumeBar.Location = new System.Drawing.Point(13, 74);
             this.volumeBar.Maximum = 100;
+            this.volumeBar.MaximumSize = new System.Drawing.Size(220, 30);
+            this.volumeBar.MinimumSize = new System.Drawing.Size(215, 30);
             this.volumeBar.Name = "volumeBar";
-            this.volumeBar.Size = new System.Drawing.Size(201, 45);
+            this.volumeBar.Size = new System.Drawing.Size(220, 30);
             this.volumeBar.SmallChange = 5;
             this.volumeBar.TabIndex = 3;
             this.volumeBar.TickFrequency = 10;
+            this.volumeBar.TickStyle = System.Windows.Forms.TickStyle.None;
             this.volumeBar.Value = 100;
             this.volumeBar.Scroll += new System.EventHandler(this.volumeBar_Scroll);
             // 
             // playlistBox
             // 
+            this.playlistBox.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.playlistBox.FormattingEnabled = true;
-            this.playlistBox.Location = new System.Drawing.Point(24, 107);
+            this.playlistBox.Location = new System.Drawing.Point(0, 62);
             this.playlistBox.Name = "playlistBox";
-            this.playlistBox.Size = new System.Drawing.Size(189, 21);
+            this.playlistBox.Size = new System.Drawing.Size(200, 21);
             this.playlistBox.TabIndex = 5;
-
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.artistAlbumLabel);
+            this.panel1.Controls.Add(this.songLabel);
+            this.panel1.Controls.Add(this.playlistBox);
+            this.panel1.Location = new System.Drawing.Point(244, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 83);
+            this.panel1.TabIndex = 6;
+            // 
+            // songLabel
+            // 
+            this.songLabel.AutoSize = true;
+            this.songLabel.Location = new System.Drawing.Point(10, 10);
+            this.songLabel.MinimumSize = new System.Drawing.Size(180, 0);
+            this.songLabel.Name = "songLabel";
+            this.songLabel.Size = new System.Drawing.Size(180, 13);
+            this.songLabel.TabIndex = 6;
+            this.songLabel.Text = "Song goes here";
+            this.songLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // artistAlbumLabel
+            // 
+            this.artistAlbumLabel.AutoSize = true;
+            this.artistAlbumLabel.Location = new System.Drawing.Point(10, 40);
+            this.artistAlbumLabel.MinimumSize = new System.Drawing.Size(180, 10);
+            this.artistAlbumLabel.Name = "artistAlbumLabel";
+            this.artistAlbumLabel.Size = new System.Drawing.Size(180, 13);
+            this.artistAlbumLabel.TabIndex = 7;
+            this.artistAlbumLabel.Text = "Artist - Album";
+            this.artistAlbumLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(227, 159);
-            this.Controls.Add(this.playlistBox);
+            this.ClientSize = new System.Drawing.Size(456, 113);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.volumeBar);
             this.Controls.Add(this.nextButton);
             this.Controls.Add(this.prevButton);
@@ -116,6 +160,8 @@
             this.Text = "Music Shuffler";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainWindow_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -139,6 +185,10 @@
             this.playlistBox.SelectedValueChanged += new System.EventHandler(this.playlistBox_SelectedValueChanged);
             playlistBoxLastIndex = playlistBox.SelectedIndex;
         }
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label artistAlbumLabel;
+        private System.Windows.Forms.Label songLabel;
     }
 }
 
